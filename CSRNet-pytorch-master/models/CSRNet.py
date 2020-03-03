0f88a803c532ac22cc8870f6b62577d668a9d206
aubrydandoque <aubry@dandoque.com>
2020-03-03 17:33:39 +0100

    Modified Model as classes

--- CSRNet-pytorch-master/models/CSRNet.py
+++ CSRNet-pytorch-master/models/CSRNet.py
@@ -1,24 +1,27 @@
 import torch.nn as nn
 import torch
 from torchvision import models
-from utils import save_net, load_net
+
+"""
+    2 Classes to create a complete CSRNet Model
+"""
 
 
-class CSRNet(nn.Module):
-    def __init__(self, load_weights=False):
-        super(CSRNet, self).__init__()
-        self.seen = 0
-        # int for each Conv Layer, M for each MaxPool Layer
+class CSRNet_Front(nn.Module):
+    """Front Layers of a CSNet model
+
+        Args:
+        pre_trained (bool): If True, load layers from VGG
+    """
+
+    def __init__(self, pre_trained=True):
+        super(CSRNet_Front, self).__init__()
         self.frontend_feat = [64, 64, 'M', 128, 128,
                               'M', 256, 256, 256, 'M', 512, 512, 512]
-        self.backend_feat = [512, 512, 512, 256, 128, 64]
         self.frontend = make_layers(self.frontend_feat)
-        self.backend = make_layers(
-            self.backend_feat, in_channels=512, dilation=True)
-        self.output_layer = nn.Conv2d(64, 1, kernel_size=1)
 
-        # Initialize frontend layers with VGG
-        if not load_weights:
+        # If pre-trained load weights from VGG
+        if pre_trained:
             mod = models.vgg16(pretrained=True)
             self._initialize_weights()
             for i in xrange(len(self.frontend.state_dict().items())):
@@ -27,8 +30,6 @@ class CSRNet(nn.Module):
 
     def forward(self, x):
         x = self.frontend(x)
-        x = self.backend(x)
-        x = self.output_layer(x)
         return x
 
     def _initialize_weights(self):
@@ -42,21 +43,42 @@ class CSRNet(nn.Module):
                 nn.init.constant_(m.bias, 0)
 
 
-def make_layers(cfg, in_channels=3, batch_norm=False, dilation=False):
-    # Dilation always 1 or 2
-    if dilation:
-        d_rate = 2
-    else:
-        d_rate = 1
+class CSRNet_Back(nn.Module):
+    """Backend Layers of a CSNet model
+
+        Args:
+        dilation (List): If given, the dilation rate applied for each layer of the backend, else 1
+    """
+
+    def __init__(self, dilation=None):
+        super(CSRNet_Back, self).__init__()
+
+        self.backend_feat = [512, 512, 512, 256, 128, 64]
+        self.dilation = dilation
+
+        self.backend = make_layers(
+            self.backend_feat, in_channels=512, dilation=self.dilation)
+        self.output_layer = nn.Conv2d(64, 1, kernel_size=1)
+
+    def forward(self, x):
+        x = self.backend(x)
+        x = self.output_layer(x)
+        return x
+
+
+def make_layers(cfg, in_channels=3, batch_norm=False, dilation=None):
+    if dilation == None:
+        dilation = [1]*len(cfg)
     layers = []
-    for v in cfg:
+    for v, d_rate in zip(cfg, dilation):
         if v == 'M':
             layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
         else:
             conv2d = nn.Conv2d(in_channels, v, kernel_size=3,
                                padding=d_rate, dilation=d_rate)
             if batch_norm:
-                layers += [conv2d, nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
+                layers += [conv2d,
+                           nn.BatchNorm2d(v), nn.ReLU(inplace=True)]
             else:
                 layers += [conv2d, nn.ReLU(inplace=True)]
             in_channels = v
